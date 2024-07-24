13d153c3f461d958f621e4829b1784af6295151f
Conejo666 <sammiller0013@gmail.com>
2024-07-24 10:42:39 -0400

    set up env + store connection string in config

--- BackEndDevChallenge/CalculatorContext.cs
+++ BackEndDevChallenge/CalculatorContext.cs
@@ -1,4 +1,4 @@
-﻿using BackEndDevChallenge.Models;
+﻿    using BackEndDevChallenge.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 
@@ -6,14 +6,23 @@ namespace BackEndDevChallenge
 {
     public class CalculatorContext : DbContext
     {
-        // This is the connection string to the database
-        // Please change this to your own connection string for testing.
-        public static string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MathDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        private readonly IConfiguration _configuration;
+
+        public CalculatorContext(DbContextOptions<CalculatorContext> options, IConfiguration configuration)
+            : base(options)
+        {
+            _configuration = configuration;
+        }
+
         public DbSet<MathProblem> MathProblems { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(ConnectionString);
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = _configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
         }
     }
 }
